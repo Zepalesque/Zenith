6c4dda1e2f70581ed61e2dca5b2beac3765d6d16
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-05-26 12:07:10 -0400

    feat: Documentation stuff

--- src/main/java/net/zepalesque/zenith/api/blockset/util/CraftingMatrix.java
+++ src/main/java/net/zepalesque/zenith/api/blockset/util/CraftingMatrix.java
@@ -8,6 +8,10 @@ public class CraftingMatrix {
 
     protected final UnaryOperator<ShapedRecipeBuilder> operation;
 
+    /**
+     * A matrix for {@link net.zepalesque.zenith.api.blockset.AbstractStoneSet} crafting.
+     * @param pattern The pattern of the recipe, should use hashtag/number symbols (#) only.
+     */
     public CraftingMatrix(String... pattern) {
         this(builder -> {
             if (pattern.length > 3) {
@@ -19,9 +23,16 @@ public class CraftingMatrix {
             return builder;
         });
     }
+
+    /**
+     * Direct constructor to perform a unary operation on a {@link ShapedRecipeBuilder}.
+     * @param operation the {@link UnaryOperator} that should be performed. Can be used for more complex crafting behavior
+     */
     public CraftingMatrix(UnaryOperator<ShapedRecipeBuilder> operation) {
         this.operation = operation;
     }
 
-
+    public ShapedRecipeBuilder apply(ShapedRecipeBuilder builder) {
+        return this.operation.apply(builder);
+    }
 }
