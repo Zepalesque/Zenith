79e8d671d00dec29e3cb556dde230bbf18ad87be
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-06-11 19:04:05 -0400

    fix: forgor, now 1.0.44 time

--- src/main/java/net/zepalesque/zenith/recipe/builder/StackingRecipeBuilder.java
+++ src/main/java/net/zepalesque/zenith/recipe/builder/StackingRecipeBuilder.java
@@ -1,25 +1,26 @@
 package net.zepalesque.zenith.recipe.builder;
 
 import net.minecraft.advancements.Criterion;
+import net.minecraft.core.Holder;
 import net.minecraft.data.recipes.RecipeBuilder;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.zepalesque.zenith.recipe.recipes.AbstractStackingRecipe;
-import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
-import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Optional;
 
 public class StackingRecipeBuilder implements RecipeBuilder {
     private final ItemStack result;
     private final Ingredient ingredient;
-    private Optional<CompoundTag> additionalData = Optional.empty();
+    private Optional<CompoundTag> extra = Optional.empty();
+    private Optional<Holder<SoundEvent>> sound = Optional.empty();
     private final AbstractStackingRecipe.Factory<?> factory;
 
     public StackingRecipeBuilder(ItemStack result, Ingredient ingredient, AbstractStackingRecipe.Factory<?> factory) {
@@ -39,7 +40,12 @@ public class StackingRecipeBuilder implements RecipeBuilder {
     }
 
     public RecipeBuilder withExtra(CompoundTag data) {
-        this.additionalData = Optional.of(data);
+        this.extra = Optional.of(data);
+        return this;
+    }
+
+    public RecipeBuilder withSound(Holder<SoundEvent> sound) {
+        this.sound = Optional.of(sound);
         return this;
     }
 
@@ -63,7 +69,7 @@ public class StackingRecipeBuilder implements RecipeBuilder {
 
     @Override
     public void save(RecipeOutput output, ResourceLocation id) {
-        AbstractStackingRecipe recipe = this.factory.create(this.getIngredient(), this.getResultStack(), this.additionalData);
+        AbstractStackingRecipe recipe = this.factory.create(this.getIngredient(), this.getResultStack(), this.extra);
         output.accept(id, recipe, null);
     }
 }
