f1b79672505fb88e8682d6d38cb252f396fe634d
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-06-18 18:26:53 -0400

    fix: imports

--- src/main/java/net/zepalesque/zenith/world/structure/modifier/ZenithStructureModifiers.java
+++ src/main/java/net/zepalesque/zenith/world/structure/modifier/ZenithStructureModifiers.java
@@ -1,17 +1,11 @@
 package net.zepalesque.zenith.world.structure.modifier;
 
 import com.mojang.serialization.Codec;
-import net.neoforged.neoforge.common.world.BiomeModifier;
 import net.neoforged.neoforge.common.world.StructureModifier;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import net.zepalesque.zenith.Zenith;
-import net.zepalesque.zenith.world.biome.modifier.ConditionalBiomeModifier;
-import net.zepalesque.zenith.world.biome.modifier.GrassFoliageModifier;
-import net.zepalesque.zenith.world.biome.modifier.MusicModifier;
-import net.zepalesque.zenith.world.biome.modifier.SkyModifier;
-import net.zepalesque.zenith.world.biome.modifier.WaterColorModifier;
 
 public class ZenithStructureModifiers {
     public static final DeferredRegister<Codec<? extends StructureModifier>> CODECS = DeferredRegister.create(NeoForgeRegistries.STRUCTURE_MODIFIER_SERIALIZERS, Zenith.MODID);
