7ab6d710cf706fe7110890af6c61964b9e48a4d8
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-06-20 20:53:10 -0400

    feat: Surface Rule Lake

--- src/main/java/net/zepalesque/zenith/world/feature/gen/ZenithFeatures.java
+++ src/main/java/net/zepalesque/zenith/world/feature/gen/ZenithFeatures.java
@@ -10,4 +10,6 @@ public class ZenithFeatures {
     public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(BuiltInRegistries.FEATURE, Zenith.MODID);
 
     public static DeferredHolder<Feature<?>, Feature<BlockWithPredicateFeature.Config>> BLOCK_WITH_PREDICATE = FEATURES.register("block_with_predicate", () -> new BlockWithPredicateFeature(BlockWithPredicateFeature.Config.CODEC));
+    public static DeferredHolder<Feature<?>, Feature<SurfaceRuleLakeFeature.Config>> SURFACE_RULE_LAKE = FEATURES.register("surface_rule_lake", () -> new SurfaceRuleLakeFeature(SurfaceRuleLakeFeature.Config.CODEC));
+
 }
