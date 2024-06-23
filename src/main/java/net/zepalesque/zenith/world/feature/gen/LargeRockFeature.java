2b87aac616fb420ccd7022210bb56df108ff4eda
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-06-23 12:20:02 -0400

    fix: fix

--- src/main/java/net/zepalesque/zenith/world/feature/gen/LargeRockFeature.java
+++ src/main/java/net/zepalesque/zenith/world/feature/gen/LargeRockFeature.java
@@ -62,7 +62,9 @@ public class LargeRockFeature extends Feature<LargeRockFeature.Config> {
                 BlockPos imm1 = origin.relative(d);
                 if (i < 2 || (potentiallyPlaceAbove.contains(d) && rand.nextFloat() < 0.7)) {
                     setBlock(imm1.above(i), context);
-                    placedAbove.add(d);
+                    if (i == 2) {
+                        placedAbove.add(d);
+                    }
                 }
             }
         }
