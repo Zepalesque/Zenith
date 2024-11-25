cb80434d91b4d332f5eb54d33ad0473c54af6b01
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-11-25 18:46:32 -0500

    fix: we gaming

--- src/main/java/net/zepalesque/zenith/tile/ZenithSignBlockEntity.java
+++ src/main/java/net/zepalesque/zenith/tile/ZenithSignBlockEntity.java
@@ -8,13 +8,8 @@ import net.minecraft.world.level.block.state.BlockState;
 import java.util.function.Supplier;
 
 public class ZenithSignBlockEntity extends SignBlockEntity {
-    private final Supplier<BlockEntityType<? extends ZenithSignBlockEntity>> type;
-    public ZenithSignBlockEntity(BlockPos pos, BlockState state, Supplier<BlockEntityType<? extends ZenithSignBlockEntity>> type) {
-        super(pos, state);
-        this.type = type;
-    }
 
-    public BlockEntityType<? extends ZenithSignBlockEntity> getType() {
-        return this.type.get();
+    public ZenithSignBlockEntity(BlockPos pos, BlockState state, Supplier<BlockEntityType<? extends ZenithSignBlockEntity>> type) {
+        super(type.get(), pos, state);
     }
 }
