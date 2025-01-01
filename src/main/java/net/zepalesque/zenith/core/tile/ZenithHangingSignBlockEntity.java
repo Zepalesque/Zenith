c566ef8f980f81d7ea765e3433b7c8b90e927095
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2025-01-01 18:18:39 -0500

    fix: STATIC AAAAAAAAAAA

--- src/main/java/net/zepalesque/zenith/core/tile/ZenithHangingSignBlockEntity.java
+++ src/main/java/net/zepalesque/zenith/core/tile/ZenithHangingSignBlockEntity.java
@@ -9,7 +9,6 @@ import net.zepalesque.zenith.api.blockset.type.AbstractWoodSet;
 import java.util.function.Supplier;
 
 public class ZenithHangingSignBlockEntity extends HangingSignBlockEntity {
-
     protected final AbstractWoodSet woodset;
 
     protected <T extends AbstractWoodSet> ZenithHangingSignBlockEntity(BlockPos pos, BlockState state, T woodset) {
@@ -17,8 +16,8 @@ public class ZenithHangingSignBlockEntity extends HangingSignBlockEntity {
         this.woodset = woodset;
     }
 
-        public <T extends AbstractWoodSet> ZenithHangingSignBlockEntity create(BlockPos pos, BlockState state, T set) {
-
+    // Fix to get around crashing caused by the validateBlockState method
+    public static <T extends AbstractWoodSet> ZenithHangingSignBlockEntity create(BlockPos pos, BlockState state, T set) {
         return new ZenithHangingSignBlockEntity(pos, state, set) {
             @Override
             public BlockEntityType<? extends ZenithHangingSignBlockEntity> getType() {
