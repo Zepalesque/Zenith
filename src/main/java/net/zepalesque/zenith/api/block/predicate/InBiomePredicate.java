eba384ec9d025de96f861455a91373f60f239d70
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2025-01-16 19:59:11 -0500

    fix: allow tag usage

--- src/main/java/net/zepalesque/zenith/api/block/predicate/InBiomePredicate.java
+++ src/main/java/net/zepalesque/zenith/api/block/predicate/InBiomePredicate.java
@@ -1,23 +1,24 @@
 package net.zepalesque.zenith.api.block.predicate;
 
+import com.mojang.datafixers.util.Either;
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.RegistryCodecs;
-import net.minecraft.core.Vec3i;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
 import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicateType;
-import net.minecraft.world.phys.shapes.Shapes;
 import net.zepalesque.zenith.core.registry.ZenithBlockPredicates;
 
-public record InBiomePredicate(HolderSet<Biome> biomes) implements BlockPredicate {
+public record InBiomePredicate(Either<HolderSet<Biome>, TagKey<Biome>> biomes) implements BlockPredicate {
     public static MapCodec<InBiomePredicate> CODEC = RecordCodecBuilder.mapCodec(builder -> builder.group(
-                    RegistryCodecs.homogeneousList(Registries.BIOME).fieldOf("biomes").forGetter(InBiomePredicate::biomes))
+                    Codec.either(RegistryCodecs.homogeneousList(Registries.BIOME), TagKey.hashedCodec(Registries.BIOME)).fieldOf("biomes").forGetter(InBiomePredicate::biomes))
             .apply(builder, InBiomePredicate::new)
     );
 
@@ -27,6 +28,15 @@ public record InBiomePredicate(HolderSet<Biome> biomes) implements BlockPredicat
     }
 
     public boolean test(WorldGenLevel level, BlockPos pos) {
-        return this.biomes.contains(level.getBiome(pos));
+        Holder<Biome> biome = level.getBiome(pos);
+        return this.biomes.map(holders -> holders.contains(biome), biome::is);
+    }
+
+    public static InBiomePredicate inTag(TagKey<Biome> tag) {
+        return new InBiomePredicate(Either.right(tag));
+    }
+
+    public static InBiomePredicate inSet(HolderSet<Biome> set) {
+        return new InBiomePredicate(Either.left(set));
     }
 }
