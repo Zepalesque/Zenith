1baaa830cc3cecf5b67a11a076fee0ed350545f8
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2025-03-03 17:09:46 -0500

    feat:  aaaaaa a

--- src/main/java/net/zepalesque/zenith/util/function/Functions.java
+++ src/main/java/net/zepalesque/zenith/util/function/Functions.java
@@ -10,12 +10,12 @@ import java.util.function.Function;
  * @see java.util.function.BiFunction BiFunction
  */
 public class Functions {
-
+    
     /**
      * A function with three inputs. Longer name would be {@code 'TriFunction'.}
      */
     public interface F3<T1, T2, T3, R> {
-
+        
         /**
          * Applies this function to the given arguments.
          *
@@ -25,15 +25,15 @@ public class Functions {
          * @return the function result
          */
         R apply(T1 t1, T2 t2, T3 t3);
-
+        
         /**
          * Returns a composed function that first applies this function to
          * its input, and then applies the {@code after} function to the result.
          * If evaluation of either function throws an exception, it is relayed to
          * the caller of the composed function.
          *
-         * @param <V> the type of output of the {@code after} function, and of the
-         *           composed function
+         * @param <V>   the type of output of the {@code after} function, and of the
+         *              composed function
          * @param after the function to apply after this function is applied
          * @return a composed function that first applies this function and then
          * applies the {@code after} function
@@ -42,12 +42,12 @@ public class Functions {
             return (t1, t2, t3) -> after.apply(apply(t1, t2, t3));
         }
     }
-
+    
     /**
      * A function with four inputs. Longer name would be {@code 'QuatFunction'.}
      */
     public interface F4<T1, T2, T3, T4, R> {
-
+        
         /**
          * Applies this function to the given arguments.
          *
@@ -58,15 +58,15 @@ public class Functions {
          * @return the function result
          */
         R apply(T1 t1, T2 t2, T3 t3, T4 t4);
-
+        
         /**
          * Returns a composed function that first applies this function to
          * its input, and then applies the {@code after} function to the result.
          * If evaluation of either function throws an exception, it is relayed to
          * the caller of the composed function.
          *
-         * @param <V> the type of output of the {@code after} function, and of the
-         *           composed function
+         * @param <V>   the type of output of the {@code after} function, and of the
+         *              composed function
          * @param after the function to apply after this function is applied
          * @return a composed function that first applies this function and then
          * applies the {@code after} function
@@ -75,12 +75,12 @@ public class Functions {
             return (t1, t2, t3, t4) -> after.apply(apply(t1, t2, t3, t4));
         }
     }
-
+    
     /**
      * A function with five inputs. Longer name would be {@code 'QuinFunction'.}
      */
     public interface F5<T1, T2, T3, T4, T5, R> {
-
+        
         /**
          * Applies this function to the given arguments.
          *
@@ -92,15 +92,15 @@ public class Functions {
          * @return the function result
          */
         R apply(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5);
-
+        
         /**
          * Returns a composed function that first applies this function to
          * its input, and then applies the {@code after} function to the result.
          * If evaluation of either function throws an exception, it is relayed to
          * the caller of the composed function.
          *
-         * @param <V> the type of output of the {@code after} function, and of the
-         *           composed function
+         * @param <V>   the type of output of the {@code after} function, and of the
+         *              composed function
          * @param after the function to apply after this function is applied
          * @return a composed function that first applies this function and then
          * applies the {@code after} function
@@ -109,12 +109,12 @@ public class Functions {
             return (t1, t2, t3, t4, t5) -> after.apply(apply(t1, t2, t3, t4, t5));
         }
     }
-
+    
     /**
      * A function with six inputs. Longer name would be {@code 'HexFunction'.}
      */
     public interface F6<T1, T2, T3, T4, T5, T6, R> {
-
+        
         /**
          * Applies this function to the given arguments.
          *
@@ -127,15 +127,15 @@ public class Functions {
          * @return the function result
          */
         R apply(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6);
-
+        
         /**
          * Returns a composed function that first applies this function to
          * its input, and then applies the {@code after} function to the result.
          * If evaluation of either function throws an exception, it is relayed to
          * the caller of the composed function.
          *
-         * @param <V> the type of output of the {@code after} function, and of the
-         *           composed function
+         * @param <V>   the type of output of the {@code after} function, and of the
+         *              composed function
          * @param after the function to apply after this function is applied
          * @return a composed function that first applies this function and then
          * applies the {@code after} function
@@ -144,12 +144,12 @@ public class Functions {
             return (t1, t2, t3, t4, t5, t6) -> after.apply(apply(t1, t2, t3, t4, t5, t6));
         }
     }
-
+    
     /**
      * A function with seven inputs. Longer name would be {@code 'SeptFunction'.}
      */
     public interface F7<T1, T2, T3, T4, T5, T6, T7, R> {
-
+        
         /**
          * Applies this function to the given arguments.
          *
@@ -163,15 +163,15 @@ public class Functions {
          * @return the function result
          */
         R apply(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7);
-
+        
         /**
          * Returns a composed function that first applies this function to
          * its input, and then applies the {@code after} function to the result.
          * If evaluation of either function throws an exception, it is relayed to
          * the caller of the composed function.
          *
-         * @param <V> the type of output of the {@code after} function, and of the
-         *           composed function
+         * @param <V>   the type of output of the {@code after} function, and of the
+         *              composed function
          * @param after the function to apply after this function is applied
          * @return a composed function that first applies this function and then
          * applies the {@code after} function
@@ -180,12 +180,12 @@ public class Functions {
             return (t1, t2, t3, t4, t5, t6, t7) -> after.apply(apply(t1, t2, t3, t4, t5, t6, t7));
         }
     }
-
+    
     /**
      * A function with eight inputs. Longer name would be {@code 'OctFunction'.}
      */
     public interface F8<T1, T2, T3, T4, T5, T6, T7, T8, R> {
-
+        
         /**
          * Applies this function to the given arguments.
          *
@@ -200,15 +200,15 @@ public class Functions {
          * @return the function result
          */
         R apply(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8);
-
+        
         /**
          * Returns a composed function that first applies this function to
          * its input, and then applies the {@code after} function to the result.
          * If evaluation of either function throws an exception, it is relayed to
          * the caller of the composed function.
          *
-         * @param <V> the type of output of the {@code after} function, and of the
-         *           composed function
+         * @param <V>   the type of output of the {@code after} function, and of the
+         *              composed function
          * @param after the function to apply after this function is applied
          * @return a composed function that first applies this function and then
          * applies the {@code after} function
@@ -216,5 +216,76 @@ public class Functions {
         default <V> F8<T1, T2, T3, T4, T5, T6, T7, T8, V> andThen(@NotNull Function<? super R, ? extends V> after) {
             return (t1, t2, t3, t4, t5, t6, t7, t8) -> after.apply(apply(t1, t2, t3, t4, t5, t6, t7, t8));
         }
+    }
+    
+    /**
+     * A function that takes an {@code int} as an input and returns a {@code byte}.
+     */
+    public interface Int2ByteFunc {
+        
+        /**
+         * Applies this function to the given argument.
+         *
+         * @param value the function argument
+         * @return the function result
+         */
+        byte applyAsByte(int value);
+    }
+    
+    /**
+     * A function that takes an {@code int} as an input and returns a {@code short}.
+     */
+    public interface Int2ShortFunc {
+        
+        /**
+         * Applies this function to the given argument.
+         *
+         * @param value the function argument
+         * @return the function result
+         */
+        short applyAsShort(int value);
+    }
+    
+    /**
+     * A function that takes an {@code int} as an input and returns a {@code float}.
+     */
+    public interface Int2FloatFunc {
+        
+        /**
+         * Applies this function to the given argument.
+         *
+         * @param value the function argument
+         * @return the function result
+         */
+        float applyAsFloat(int value);
+    }
+    
+    /**
+     * A function that takes an {@code int} as an input and returns a {@code boolean}.
+     */
+    public interface Int2BoolFunc {
+        
+        /**
+         * Applies this function to the given argument.
+         *
+         * @param value the function argument
+         * @return the function result
+         */
+        boolean applyAsBoolean(int value);
+        
+    }
+    
+    /**
+     * A function that takes an {@code int} as an input and returns a {@code char}.
+     */
+    public interface Int2CharFunc {
+        
+        /**
+         * Applies this function to the given argument.
+         *
+         * @param value the function argument
+         * @return the function result
+         */
+        char applyAsChar(int value);
     }
 }
